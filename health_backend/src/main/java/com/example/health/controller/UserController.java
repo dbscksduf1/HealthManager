0fd4637e530110a073f1a52778dd4dbeccfe0dbd
Chanyeol Yun <soo5586068@gmail.com>
2025-12-16 20:23:38 +0900

    as

--- health_backend/src/main/java/com/example/health/controller/UserController.java
+++ health_backend/src/main/java/com/example/health/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Profile("prod")
+
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
--- health_backend/src/main/java/com/example/health/service/UserService.java
+++ health_backend/src/main/java/com/example/health/service/UserService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Profile("prod")
+
 @Service
 public class UserService {
 
