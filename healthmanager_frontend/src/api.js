ce5eb1f0412225d1ef64fb8c209f0936993bc17d
Chanyeol Yun <soo5586068@gmail.com>
2026-02-23 14:37:23 +0900

    update backend url

--- healthmanager_frontend/src/api.js
+++ healthmanager_frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://healthmanager-backend.onrender.com",
+  baseURL: "https://healthmanager-dxh7.onrender.com",
 });
 
 // ========================================
